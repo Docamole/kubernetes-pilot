ac29b441a8e3fc99aa3d7f4aa7f805c5f32ade8a
DivXZero <silv3rlogic@gmail.com>
2018-10-16 07:14:09 -0400

    Add in memory cache

--- dashboard/src/js/lib/PilotClient/PilotClient.jsx
+++ dashboard/src/js/lib/PilotClient/PilotClient.jsx
@@ -1,6 +1,6 @@
-import ApolloClient from 'apollo-boost'
+import ApolloClient, { InMemoryCache, HttpLink } from 'apollo-boost'
 
-import typeDefs from './typeDefs.graphql'
+import clientTypes from './clientTypes.graphql'
 
 const defaults = {
   workspace: {
@@ -9,24 +9,30 @@ const defaults = {
   }
 }
 
-const resolvers = {
-  Query: {
-    activeNamespace: (args, vars, { cache }) => {
-      const namespace = cache.data.data['$ROOT_QUERY.workspace'].activeNamespace
-      return namespace
-    }
-  }
-}
+// FIXME: Try passing an ApolloLink built from HttpLink
+// (note: need to move ApolloClient back into the brackets to import core client)
 
 class PilotClient {
   constructor() {
+    // this.link = new HttpLink()
+    this.cache = new InMemoryCache()
     this.client = new ApolloClient({
+      // link: this.link,
+      cache: this.cache,
       clientState: {
         defaults,
-        resolvers,
-        typeDefs
+        resolvers: this.resolvers(),
+        typeDefs: clientTypes
       }
     })
+  }
+
+  resolvers() {
+    return {
+      Query: {
+        activeNamespace: () => this.cache.data.data['$ROOT_QUERY.workspace'].activeNamespace
+      }
+    }
   }
 }
 
